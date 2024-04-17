a0423c652f48dccad9f0b54c37dbc13c37029989
yandrade1305 <yan13052001@gmail.com>
2024-04-17 12:52:52 -0300

    Subindo projeto

---
+++ src/main/java/com/stocksapi/StocksApiApplication.java
@@ -0,0 +1,13 @@
+package com.stocksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksApiApplication.class, args);
+	}
+
+}
